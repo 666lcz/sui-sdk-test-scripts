f2ff6fc64734da7c05bda9dfd157d1e3a148ecc9
Chris Li <chris@mystenlabs.com>
2023-04-03 01:36:50 -0400

    update to 0.30.0

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
-const { JsonRpcProvider, Ed25519Keypair, Network, RawSigner, LocalTxnDataSerializer } = require("@mysten/sui.js");
+const { JsonRpcProvider, Ed25519Keypair, devnetConnection } = require("@mysten/sui.js");
 
-const SEED = [
-  180, 107, 26, 32, 169, 88, 248, 46, 88, 100, 108, 243, 255, 87, 146, 92, 42,
-  147, 104, 2, 39, 200, 114, 145, 37, 122, 8, 37, 170, 238, 164, 236,
-];
-
-const keypair = Ed25519Keypair.fromSeed(new Uint8Array(SEED));
+const USELESS_MNEMOINCS = "runway arrest offer ecology erase castle stone exile point depart vintage mechanic";
+const keypair = Ed25519Keypair.deriveKeypair(USELESS_MNEMOINCS);
 const address = keypair.getPublicKey().toSuiAddress();
-const provider = new JsonRpcProvider(Network.DEVNET);
+const provider = new JsonRpcProvider(devnetConnection);
 
 
 async function main() {
-  //await provider.requestSuiFromFaucet(address);
-  const signer = new RawSigner(
-    keypair,
-    provider,
-    new LocalTxnDataSerializer(provider)
-  );
-  const result = await signer.signAndExecuteTransaction({
-    kind: 'moveCall',
-    data: {
-      packageObjectId: '0x2',
-      module: 'devnet_nft',
-      function: 'mint',
-      typeArguments: [],
-      arguments: [
-        'Example NFT',
-        'An NFT created by the wallet Command Line Tool',
-        'ipfs://bafkreibngqhl3gaa7daob4i2vccziay2jjlp435cf66vhono7nrvww53ty',
-      ],
-      gasBudget: 1000,
-    },
-  });
-  console.log(result);
+  console.log(address);
 }
 
 main().catch(console.error);
